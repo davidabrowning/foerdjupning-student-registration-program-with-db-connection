7ef8a4a97f83e666ec88c591ec6547b1da844f6c
David Browning <david.a.browning@gmail.com>
2025-03-23 22:11:43 +0100

    Repeat prompts when user enters empty strings.

--- StudentRegistrationProgramWithDBConnection/Interfaces/IInput.cs
+++ StudentRegistrationProgramWithDBConnection/Interfaces/IInput.cs
@@ -2,9 +2,7 @@
 {
     internal interface IInput
     {
-        string GetStringInput();
         string GetStringInput(string prompt);
-        int GetIntInput();
         int GetIntInput(string prompt);
     }
 }
